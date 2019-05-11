a66c40ba8e3b056970a8bf29c59699fa4f787dda
William <wpdcastro@gmail.com>
2019-05-10 21:08:08 -0300

    Infra sqlServer

---
+++ CCT.Infra/Contexts/StoreCCT.cs
@@ -0,0 +1,23 @@
+﻿using CCT.Domain.UserBC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDCommerce.Infra.Contexts
+{
+    public class StoreCCT : DbContext
+    {
+        public StoreCCT() : base(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog = CCTBD; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        {
+           
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Conquest> Conquests { get; set; }
+        public DbSet<Perfil> Perfis { get; set; }
+
+    }
+}
